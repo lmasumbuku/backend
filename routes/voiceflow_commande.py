f5bc6b4ebdb36a4844832cde28792162d870c66a
lmasumbuku <l.masumbuku@gmail.com>
2025-04-11 07:06:15 +0200

    Create voiceflow_commande.py

---
+++ routes/voiceflow_commande.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Request
+
+router = APIRouter()
+
+@router.post("/commande")
+async def recevoir_commande(request: Request):
+    data = await request.json()
+    print("✅ Nouvelle commande reçue depuis Voiceflow :", data)
+    return {"status": "ok", "recu": data}
