5ef538ab16723e435120efc0f69bbba0a30ab826
lmasumbuku <l.masumbuku@gmail.com>
2025-04-11 10:53:55 +0200

    Update voiceflow_commande.py

--- routes/voiceflow_commande.py
+++ routes/voiceflow_commande.py
@@ -4,6 +4,7 @@ router = APIRouter()
 
 @router.post("/commande")
 async def recevoir_commande(request: Request):
+    print("✅ Requête reçue")
     data = await request.json()
     print("✅ Nouvelle commande reçue depuis Voiceflow :", data)
     return {"status": "ok", "recu": data}
