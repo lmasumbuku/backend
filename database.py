6040ddb50334985a68fe024d180df8acb8da7b27
lmasumbuku <l.masumbuku@gmail.com>
2025-04-22 18:46:51 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bdd_restaurant_user:VCy0VOy9xOanCwwPg1Nk3XoaTc6cBvhP@dpg-cvdelubv2p9s73cehhvg-a.oregon-postgres.render.com/bdd_restaurant")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://bdd_restaurant_user:VCy0VOy9xOanCwwPg1Nk3XoaTc6cBvhP@dpg-cvdelubv2p9s73cehhvg-a.oregon-postgres.render.com/bdd_restaurant?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
