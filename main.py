388933707848f67c869c03b7060be4517efd48ce
lmasumbuku <l.masumbuku@gmail.com>
2025-09-17 00:39:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from routes.status import router as status_router
 from routes import secure_routes
 from routes.init_debug import router as init_debug_router
 from database import Base, engine
-from routes.voice import voice_router
+from voice import voice
 
 create_tables_if_not_exist()
 
